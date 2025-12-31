b3f7e0853c0fc3a2ac70ee12064d4a204d373802
Kyle J. Fischer <kylefischer1995@gmail.com>
2025-12-30 17:07:22 -0700

    Removed jenny

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-JENNY_DIR="${1:-.jenny}"
+OBSIDIAN_ACTIONS_DIR="${1:-.jenny}"
 
 # Get the directory where this script is located
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Run the Python runner
-python3 "${SCRIPT_DIR}/obsidian_actions_runner.py" "${JENNY_DIR}"
+python3 "${SCRIPT_DIR}/obsidian_actions_runner.py" "${OBSIDIAN_ACTIONS_DIR}"
